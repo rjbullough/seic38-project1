5d50aba15b6a1ad83fd24202edfc13bb00cf0ea7
Ryan Bullough <rjbullough@gmail.com>
2020-10-20 19:05:04 +1100

    pre npm shit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,19 @@
 class EntriesController < ApplicationController
-  before_action :check_for_login
+
   def new
     @item = Item.new
     @entry = Entry.new
   end
 
   def create
+    @entry = Entry.create
     @item = Item.create
+  end
+
+  def update
+  end
+  
+  def edit
   end
 
 end
